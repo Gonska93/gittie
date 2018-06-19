2bd08d724d66200f808117b81dab74147e53702b
Gonska93 <gonska9933@gmail.com>
2018-06-19 10:39:12 +0200

    function smile

--- gittie.py
+++ gittie.py
@@ -13,9 +13,12 @@ def shout():
 
 def joke():
     print("Wikipedia: I know everything!\n Google: I have everything! \n Facebook: I know everybody! \n Internet: Without me you are nothing!\n Electricity: Keep talking bitches! ")
+def smile():
+    print(':)')
 
 hello_git()    
 joke()   
 print(add(3,6))
 introduce()
 shout()
+smile()

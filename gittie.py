9d779bddb15ae1c06bf3c42e8a372979ec5ae164
Barbara Bochińska <bachna22@gmail.com>
2018-06-19 10:24:30 +0200

    add WTF

--- gittie.py
+++ gittie.py
@@ -4,12 +4,14 @@ def add(a,b):
     return a + b
 
 hello_git()
-<<<<<<< HEAD
 
 def joke():
     print("Wikipedia: I know everything!\n Google: I have everything! \n Facebook: I know everybody! \n Internet: Without me you are nothing!\n Electricity: Keep talking bitches! ")
     
 joke()   
-=======
 print(add(3,6))
->>>>>>> f957665b113e4b1810e02e82d5ab012c217a62e0
+
+def introduce():
+    print("WTF?")
+
+introduce()

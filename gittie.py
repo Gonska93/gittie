48aac3365aa4a990e78c43aa7af7ee80b7ca57ab
Barbara Bochińska <bachna22@gmail.com>
2018-06-19 10:17:54 +0200

    joke function

--- gittie.py
+++ gittie.py
@@ -2,3 +2,8 @@ def hello_git():
     print("Hello,Im Gittie")
 
 hello_git()
+
+def joke():
+    print("Wikipedia: I know everything!\n Google: I have everything! \n Facebook: I know everybody! \n Internet: Without me you are nothing!\n Electricity: Keep talking bitches! ")
+    
+joke()   
